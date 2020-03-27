9accc61ad493b98cc99500bdbca7004579ebe15a
Giovanni Tommasi <giovannitommasi95@gmail.com>
2020-03-27 19:09:04 +0100

    Removed node modules

--- node_modules/@google/model-viewer/lib/template.d.ts
+++
@@ -1,3 +0,0 @@
-declare const template: HTMLTemplateElement;
-export default template;
-export declare const makeTemplate: (tagName: string) => HTMLTemplateElement;
--- node_modules/@google/model-viewer/lib/three-components/shader-chunk/alphatest_fragment.glsl.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @license MIT
- * @see https://github.com/mrdoob/three.js/blob/dev/LICENSE
- */
-export declare const alphaChunk = "\n#ifdef ALPHATEST\n\n    if ( diffuseColor.a < ALPHATEST ) discard;\n    diffuseColor.a = 1.0;\n\n#endif\n";
